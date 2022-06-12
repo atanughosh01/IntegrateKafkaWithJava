6eb3083d0804c63ae1321abccee2c028ced23bb7
atanughosh01 <62764156+atanughosh01@users.noreply.github.com>
2022-06-12 15:24:14 +0530

    added Sample Kafka Simulation integrated with Core Java

---
+++ KafkaTest/src/main/java/org/xadmin/Consumer.java
@@ -0,0 +1,64 @@
+package org.xadmin;
+
+import org.apache.kafka.clients.consumer.ConsumerConfig;
+import org.apache.kafka.clients.consumer.ConsumerRecord;
+import org.apache.kafka.clients.consumer.ConsumerRecords;
+import org.apache.kafka.clients.consumer.KafkaConsumer;
+import org.apache.kafka.common.serialization.StringDeserializer;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.time.Duration;
+import java.util.Arrays;
+import java.util.Properties;
+
+
+// Simple Consumer class for receiving the published messages from the kafka-broker
+public class Consumer {
+    public static void main(String[] args) {
+        System.out.println("Hello Kafka-Consumer");
+
+        // 1. create logger for consumer class
+        final Logger logger = LoggerFactory.getLogger(Consumer.class);
+
+        // 2. create constant-variables for strings
+        final String bootstrapServers = "localhost:9092";
+        final String consumerGroupID = "java-group-consumer";
+
+        // 3. create and populate properties object
+        Properties prop = new Properties();
+        // bootstrap.servers
+        prop.setProperty(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, bootstrapServers);
+        // key.deSerializer
+        prop.setProperty(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class.getName());
+        // value.deSerializer
+        prop.setProperty(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class.getName());
+        // group_id_Config
+        prop.setProperty(ConsumerConfig.GROUP_ID_CONFIG, consumerGroupID);
+        // offset_rest_config
+        prop.setProperty(ConsumerConfig.AUTO_OFFSET_RESET_CONFIG, "earliest");
+
+        // 4. create consumer instance
+        final KafkaConsumer<String, String> consumer = new KafkaConsumer<>(prop);
+
+        // 5. subscribe to topic(s)
+        consumer.subscribe(Arrays.asList("KafkaTest"));
+
+        // 6. poll and consume records
+        while (true) {
+            ConsumerRecords<String, String> records = consumer.poll(Duration.ofMillis(1000));
+            for (ConsumerRecord<String, String> record : records) {
+                logger.info("\n\n Received New Record." +
+                        "\n Key: " + record.key() +
+                        ",\n Value: " + record.value() +
+                        ",\n Topic: " + record.topic() +
+                        ",\n Partition: " + record.partition() +
+                        ",\n Offset: " + record.offset() +
+                        ",\n @Timestamp: " + record.timestamp() + "\n");
+            }
+        }
+
+        // 7. close the consumer
+        // consumer.close();
+    }
+}
--- KafkaTest/src/main/java/org/xadmin/Producer.java
+++ KafkaTest/src/main/java/org/xadmin/Producer.java
@@ -1,52 +1,65 @@
 package org.xadmin;
 
-import org.apache.kafka.clients.producer.KafkaProducer;
-import org.apache.kafka.clients.producer.ProducerConfig;
-import org.apache.kafka.clients.producer.ProducerRecord;
+import org.apache.kafka.clients.producer.*;
 import org.apache.kafka.common.serialization.StringSerializer;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import java.util.Properties;
 
-/*
- * Sample Producer class for publishing messages to the kafka-broker
- */
+
+// Simple Producer class for publishing messages to the kafka-broker
 public class Producer {
     public static void main(String[] args) {
+        System.out.println("Hello Kafka-Producer");
 
-        // 1. Create the properties object for Producer
+        // 1. set up a logger
+        final Logger logger = LoggerFactory.getLogger(Producer.class);
+
+        // 2. create constant-variables for strings
+        final String bootstrapServers = "localhost:9092";
+
+        // 3. Create the properties object for Producer
         Properties prop = new Properties();
         // bootstrap.servers
-        prop.setProperty(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, "localhost:9092");
+        prop.setProperty(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, bootstrapServers);
         // key.serializer
         prop.setProperty(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class.getName());
         // value.serializer
         prop.setProperty(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, StringSerializer.class.getName());
 
-        // 2. Create the Producer
+        // 4. Create the Producer
         final KafkaProducer<String, String> producer = new KafkaProducer<>(prop);
-        /**
-          * try (final KafkaProducer<String, String> producer = new KafkaProducer<String, String>(prop)) {
-          *     System.out.println("Producer Has Been Created Successfully!");
-          * } catch (Exception e) {
-          *     e.printStackTrace();
-          * }
-          */
 
-        // 3. Create the ProducerRecord
-        ProducerRecord<String, String> record = new ProducerRecord<>("KafkaTest", "Key-1", "Value-1");
+        // 5. Create the ProducerRecord
+        // ProducerRecord<String, String> record = new ProducerRecord<>("KafkaTest", "Key-1", "Value-1");
 
-        // 4. Send Data-Asynchronous
-        producer.send(record);
+        // 6. Send Data-Asynchronous
+        // producer.send(record);
 
-        /**
-          * final int itrLim = 100;
-          * for (int i=0; i<itrLim; i++) {
-          *     record = new ProducerRecord<>("KafkaTest", "Key-"+(i+1), "Value-"+(i+1));
-          *     producer.send(record);
-          * }
-          */
 
-        // 5. Flush and close the Producer
+        ProducerRecord<String, String> record = null;
+        for (int i = 1000; i < 1005; i++) {
+            record = new ProducerRecord<>("KafkaTest", "Key-" + (i + 1), "Value-" + (i + 1));
+            producer.send(record, new Callback() {
+                @Override
+                public void onCompletion(RecordMetadata recordMetadata, Exception ex) {
+                    if (ex == null) {
+                        // success case
+                        logger.info("\n\n Received Record Metadata." +
+                                "\n Topic: " + recordMetadata.topic() +
+                                ",\n Partition: " + recordMetadata.partition() +
+                                ",\n Offset: " + recordMetadata.offset() +
+                                ",\n @Timestamp: " + recordMetadata.timestamp() + "\n");
+                    } else {
+                        // when the publishable msg FAILS to get published to kafka-broker
+                        logger.error("ERROR OCCURRED : ", ex);
+                    }
+                }
+            });
+        }
+
+        // 7. Flush and close the Producer
         producer.flush();
         producer.close();
     }
